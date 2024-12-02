e0a8c7fb7c98d7220a7938af0853addf6baa2888
Khalafallah55 <mohamedkhalafallah4@gmail.com>
2024-12-02 10:34:17 +0300

    Add Changes

--- CoreApplication/Controllers/WeatherForecastController.cs
+++ CoreApplication/Controllers/WeatherForecastController.cs
@@ -1,3 +1,6 @@
+using Mcs.Masar.Documentum;
+using Mcs.Masar.Jics;
+using Mcs.Masar.workflow;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreApplication.Controllers
@@ -21,11 +24,15 @@ namespace CoreApplication.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            var Documtentum = new Documentum();
+            var workFlow = new Workflow();
+            var Jisc = new Jics();
+            
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Documtentum.DocmentmDummyValue + workFlow.WorkflowDummyValue + Jisc.DocmentmJicsValue
             })
             .ToArray();
         }
